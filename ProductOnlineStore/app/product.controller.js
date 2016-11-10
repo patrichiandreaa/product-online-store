6682651fe96173598aef76d2366e3093f8ee74db
Raluca001 <Raluca.Codreanu@ness.com>
2016-11-10 14:43:49 +0200

    filter functionality

--- ProductOnlineStore/app/product.controller.js
+++ ProductOnlineStore/app/product.controller.js
@@ -1,33 +1,84 @@
-﻿(function () {
+﻿(function() {
     var app = angular.module('app', ['ngTouch', 'ui.grid']);
 
-    app.controller("ProductController", ['$scope', function ($scope) {
-        var vm = this;
-        vm.categories =
-      [
-          { Id: 1, Name: 'Material' },
-          { Id: 2, Name: 'Food' }
-      ];
-        vm.products =
-           [
-       { Id: 1, Name: 'Product1', Quantity: 20, Price: '3$', Category: vm.categories[0].Name },
-       { Id: 2, Name: 'Product2', Quantity: 10, Price: '4$', Category: vm.categories[1].Name }
-           ];
+    app.controller("ProductController",['$scope','$timeout', function($scope,$timeout) {
+            var vm = this;
+            vm.title = "Products";
+            vm.categories = getCategories();
+            vm.products = getProducts();
+            vm.category = vm.categories[1];
+            vm.onCategoryChange = onCategoryChange;
 
-        vm.category = vm.categories[1];
+            vm.products = vm.products.filter(function (item) {
+                return (item.Category === vm.category.Name);
+            });
+        
+            function onCategoryChange($event) {
+                
+            }
+            vm.gridOptions =
+            {
+                data: vm.products,
+                columnDefs: getColumnDefinition(),
+                filterOptions: { filterText: '', useExternalFilter: true }
+            };
 
-        vm.gridOptions =
-   {
-       data: vm.products,
-       columnDefs: [
-           { field: 'Id', displayName: 'Id' },
-           { field: 'Name', displayName: 'Name' },
-           { field: 'Price', displayName: 'Price' },
-           { field: 'Quantity', displayName: 'Quantity' }]
-   };
-        vm.title = "Products";
-    }]);
+
+            function getCategories() {
+                return [
+                    { Id: 1, Name: 'Material' },
+                    { Id: 2, Name: 'Food' }
+                ];
+            }
+
+            function getProducts() {
+                return [
+                    { Id: 1, Name: 'Product1', Quantity: 20, Price: '3$', Category: vm.categories[0].Name },
+                    { Id: 2, Name: 'Product2', Quantity: 10, Price: '4$', Category: vm.categories[1].Name }
+                ];
+            }
+
+            function getColumnDefinition() {
+                return [
+                    { field: 'Id', displayName: 'Id' },
+                    { field: 'Name', displayName: 'Name' },
+                    { field: 'Price', displayName: 'Price' },
+                    { field: 'Quantity', displayName: 'Quantity' }
+                ];
+            }
+
+        }
+    ]);
 })();
+//(function () {
+//    var app = angular.module('app', ['ngTouch', 'ui.grid']);
+//    app.controller("ProductController", ['$scope', '$http', function ($scope, $http) {
+//     var vm = this;
+//        $http.get('/app/product.json')
+//                .success(function (data) {
+//                    $scope.myData = data;
+//                })
+//                .error(function (data, status, headers, config) {
+//                    console.log('error');
+//                });
+
+//        vm.filterOptions =
+//        {
+//            filterText: ''
+//        };
+//        vm.gridOptions =
+//   {
+//       data: 'product.json',
+//       columnDefs: [
+//           { field: 'Id', displayName: 'Id' },
+//           { field: 'Name', displayName: 'Name' },
+//           { field: 'Price', displayName: 'Price' },
+//           { field: 'Quantity', displayName: 'Quantity' }],
+//       filterOptions:vm.filterOptions
+//   };
+//        vm.title = "Products";
+//        }]);
+// })();
 
 
 
